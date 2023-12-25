712917ec426e2f608a810e7b203f9e73d277ec14
studiousgamer <natyavidhanbiswas10@gmail.com>
2023-12-25 23:18:16 +0530

    book management init

--- main.py
+++ main.py
@@ -8,3 +8,34 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
+def add_book():
+    pass
+
+def remove_book(id):
+    pass
+
+def search_book(id=None, name=None, author=None):
+    pass
+
+book_management = [add_book, remove_book, search_book]
+
+command = ""
+while command!="exit":
+
+    print("Library Management System")
+    print("1) Books management \n2) Book Issues \n3) Transactions")
+
+    command = input("> ")
+
+    if command == "1":
+        print("\nBooks Management")
+        print("1) Add Book \n2) Remove Books\n3) Search")
+
+        command = input("> ")
+        if command not in ["1", "2", "3"]:
+            print("Invalid Command")
+            continue
+        command = int(command)-1
+        book_management[command]()
+
